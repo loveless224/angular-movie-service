146ea12b7c85ed3de801eaae87a3ce47a730b415
loveless224 <ploveless224@gmail.com>
2024-02-18 15:45:34 -0600

    intial commit

--- src/main/java/com/paul/Controllers/MovieController.java
+++ src/main/java/com/paul/Controllers/MovieController.java
@@ -7,6 +7,7 @@ import com.paul.Services.MovieServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "api/v1/movies")
 public class MovieController {
 
